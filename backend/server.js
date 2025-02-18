dfb613f6fc09f16e81ba7adba2f4390a0a061b9b
0xfabb <vanshdev2305@gmail.com>
2025-02-18 20:10:16 +0530

    feat: Added the most basic authentication system.

--- backend/server.js
+++ backend/server.js
@@ -5,5 +5,10 @@ const PORT = process.env.PORT || 5001;
 
 
 app.use('/', HomeRoute)
+app.set("view engine", "ejs");
+app.set('views', path.resolve('../frontend/views'));
+
+
+
 
 app.listen(PORT, () => console.log(`App is listening on Port ${PORT}`));
